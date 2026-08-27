fcfaa4ed5f6025cdc5f0f0454668efbb441fea7c
M3MEMonster <1275517487@qq.com>
2026-08-27 17:17:03 +0800

    replace relative path with system env var

--- Foo_Duration_and_Subsong_Manager/fb2k_component_pack.ps1
+++ Foo_Duration_and_Subsong_Manager/fb2k_component_pack.ps1
@@ -3,8 +3,7 @@ $ComponentName = "foo_duration_subsong_manager"
 $DllName       = "$ComponentName.dll"
 $X86Dll = Join-Path $PSScriptRoot "Release\$DllName"
 $X64Dll = Join-Path $PSScriptRoot "x64\Release\$DllName"
-if (-not (Test-Path $X86Dll)) { throw "’“≤ªµΩ x86 DLL: $X86Dll`n«Îœ»±‡“Î Release | x86`nx86 DLL not found: $X86Dll`nPlease build Release | x86 first" }
-if (-not (Test-Path $X64Dll)) { throw "’“≤ªµΩ x64 DLL: $X64Dll`n«Îœ»±‡“Î Release | x64`nx64 DLL not found: $X64Dll`nPlease build Release | x64 first" }
+if (-not (Test-Path $X86Dll)) { throw "Êâæ‰∏çÂà∞ x86 DLL: $X86Dll`nËØ∑ÂÖàÁºñËØë Release | x86`nx86 DLL not found: $X86Dll`nPlease build Release | x86 first" }
 $OutDir   = Join-Path $PSScriptRoot "dist"
 $ZipPath  = Join-Path $OutDir "$ComponentName.zip"
 $Fb2kPath = Join-Path $OutDir "$ComponentName.fb2k-component"
