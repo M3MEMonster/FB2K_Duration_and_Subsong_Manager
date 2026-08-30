bb34f2d589933629d70e30f14d726fc6af890c33
M3MEMonster <1275517487@qq.com>
2026-08-30 16:55:43 +0800

    v1.1

--- Foo_Duration_and_Subsong_Manager/main.cpp
+++ Foo_Duration_and_Subsong_Manager/main.cpp
@@ -4,7 +4,7 @@
 // Since foobar2000 v1.0 having at least one of these in your DLL is mandatory to let the troubleshooter tell different versions of your component apart.
 // Note that it is possible to declare multiple components within one DLL, but it's strongly recommended to keep only one declaration per DLL.
 // As for 1.1, the version numbers are used by the component update finder to find updates; for that to work, you must have ONLY ONE declaration per DLL. If there are multiple declarations, the component is assumed to be outdated and a version number of "0" is assumed, to overwrite the component with whatever is currently on the site assuming that it comes with proper version numbers.
-DECLARE_COMPONENT_VERSION("Duration & Subsong Manager", "1.0", 
+DECLARE_COMPONENT_VERSION("Duration & Subsong Manager", "1.1", 
 	"Let users be able to customize length of any tracks\n"
 	"Let users be able to hide subsongs from the autoplaylist\n"
 	"Adds a high-precision timer UI that shows and updates playback time and track length in ms([HH:]MM:SS.mmm)");
